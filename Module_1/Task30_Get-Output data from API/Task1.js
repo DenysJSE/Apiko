e8c68f9474f779b4c20bc6dbe527f7756f5e1cf5
DenysMarkevychSE-21 <deniskomrk@gmail.com>
2023-09-26 14:02:00 +0300

    task30

---
+++ Module_1/Task30_Get-Output data from API/Task1.js
@@ -0,0 +1,19 @@
+const URL = "https://jsonplaceholder.typicode.com"
+
+fetch(`${URL}/users`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(users => {
+    console.log("Список користувачів:");
+    users.forEach(user => {
+      console.log(user);
+    });
+  })
+  .catch(error => {
+    console.error("Сталася помилка:", error);
+  });
+
